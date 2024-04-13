814588e297be7f882ea1aa3a622f9a4bb2cf674e
Bogdan <bgusuleac@gmail.com>
2024-04-13 21:30:20 +0300

    Fe + tree

---
+++ fe/src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import ToolList from "./components/tool-list";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import About from "./pages/about";
+import ProductDesigner from "./pages/product-designer";
+// import Home from "./pages/Home"
+// // ...
+// import About from "./pages/About"
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+// https://semaphoreci.com/blog/routing-layer-react#:~:text=In%20a%20single%2Dpage%20React,components%20based%20on%20user%20interaction.
+
+function App() {
+    // init routes for pages
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <ToolList/>,
+        },
+        // // other pages....
+        {
+            path: "/about",
+            element: <About />,
+        },
+        {
+            path: "/product-designer",
+            element: <ProductDesigner/>
+        }
+    ])
+
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <main>
+                <RouterProvider router={router} />
+            </main>
+        </ThemeProvider>
+    );
+}
+
+
+export default App;
