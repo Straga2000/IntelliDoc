a44b0fa90dc579981128fe15b7c7997b532f9652
Yonaioana <ioagrigore2804@yahoo.com>
2024-04-13 21:18:51 +0300

    append changes to issue

--- src/GithubIssues.py
+++ src/GithubIssues.py
@@ -8,14 +8,22 @@ def get_issue(owner, repo, token, issue_number):
     response.raise_for_status()
     return response.json()
 
-def update_dictionary(dict, updated_dict):
-    updated_dict.update(dict)
-    return updated_dict
+def update_dictionary(new_dict, old_dict):
+    # updated_dict.update(dict)
+    # return updated_dict
+    result_dict = {}
+    for key in old_dict.keys():
+            if key in new_dict and isinstance(new_dict[key],str) and isinstance(old_dict[key],str):
+             result_dict[key] = old_dict[key] + " " + new_dict[key]
+            else:
+                result_dict[key] = old_dict[key]
+    old_dict.update(result_dict)
+    return old_dict
 
 def update_issue(owner, repo, token ,issue_number):
     url = f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}"
     headers = {'Authorization': f'Bearer {token}',"Accept": "application/vnd.github+json"}
-    text = update_dictionary({"title":"test update","body":"suggestion"},get_issue("Straga2000", "Foodity","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", 1))
+    text = update_dictionary({"title":"test test test","body":"suggestion"},get_issue("Straga2000", "Foodity","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", 1))
     response = requests.patch(url, json=text,headers=headers)
     response.raise_for_status()
     return response.json()
